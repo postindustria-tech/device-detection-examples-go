a849f9e215d5bcac018aa3127571fc8d2de02dcd
Marin Miletic <mrnmiletic@gmail.com>
2024-05-09 19:36:04 +0200

    chore: simplify example

--- onpremise/automatic_update.go
+++ onpremise/automatic_update.go
@@ -8,99 +8,87 @@ import (
 
 func main() {
 	//Create config
-	config := dd.NewConfigHash(dd.Balanced)
 
-	//Create on-premise engine
-	pl, err := onpremise.New(
-		config,
-		//Provide your own file URL
-		//it can be compressed as gz or raw, engine will handle it
-		onpremise.WithDataUpdateUrl(
-			"https://myprovider.com/1.tar.gz",
-		),
-		//set custom polling interval
-		onpremise.WithPollingInterval(2),
-		// randomize polling interval, it will append random number of seconds to polling interval
-		// in case you have multiple instances of the engine running and want to avoid polling at the exact same time
-		onpremise.WithRandomization(60),
+	RunExample(
+		func(params ExampleParams) error {
+			//Create on-premise engine
+			config := dd.NewConfigHash(dd.Balanced)
+			pl, err := onpremise.New(
+				config,
+				//Provide your own file URL
+				//it can be compressed as gz or raw, engine will handle it
+				// Passing a custom update URL
+				// Path to your data file
 
-		// In case you want to use 51degrees file provider you just set licence key and product
-		onpremise.WithProduct("Hash"),
-		onpremise.WithLicenceKey("YOUR_LICENCE_KEY"),
-		//	in case you want to disable auto update
-		onpremise.WithAutoUpdate(false),
-		// in case you want to update file from URL on start of the engine otherwise it will be pulled
-		// on next polling interval
-		onpremise.WithUpdateOnStart(true),
-	)
+				onpremise.WithDataFile(params.DataFile),
+				// For automatic updates to work you will need to provide a license key.
+				// A license key can be obtained with a subscription from https://51degrees.com/pricing
+				onpremise.WithLicenceKey(params.LicenceKey),
+				onpremise.WithProduct(params.Product),
+				// Enable automatic updates.
+				onpremise.WithAutoUpdate(true),
+				// Passing a custom update URL
+				//onpremise.WithDataUpdateUrl(
+				//	"https://myprovider.com/1.tar.gz",
+				//),
+				// Enable update on startup, the auto update system
+				// will be used to check for an update before the
+				// device detection engine is created.
+				onpremise.WithUpdateOnStart(true),
 
-	if err != nil {
-		log.Fatalf("Failed to create engine: %v", err)
-	}
+				// Set the frequency in minutes that the engine should
+				// check for updates to data files. A recommended
+				// polling interval in a production environment is
+				// around 30 minutes.
+				onpremise.WithPollingInterval(1),
 
-	//Process evidence
-	resultsHash, err := pl.Process(
-		[]onpremise.Evidence{
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Arch",
-				Value:  "x86",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Model",
-				Value:  "Intel",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Mobile",
-				Value:  "?0",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Platform",
-				Value:  "Windows",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Platform-Version",
-				Value:  "10.0",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua-Full-Version-List",
-				Value:  "58.0.3029.110",
-			},
-			{
-				Prefix: dd.HttpHeaderString,
-				Key:    "Sec-Ch-Ua",
-				Value:  `"\"Chromium\";v=\"91.0.4472.124\";a=\"x86\";p=\"Windows\";rv=\"91.0\""`,
-			},
+				//set custom polling interval
+				// randomize polling interval, it will append random number of seconds to polling interval
+				// in case you have multiple instances of the engine running and want to avoid polling at the exact same time
+				onpremise.WithRandomization(60),
+
+				//File watching is enabled by default any external changes to the data file will be picked up and reloaded
+				//onpremise.WithFileWatch(true),
+
+				//TempDataCopy is enabled by default, it will copy the data file to a temporary location before loading
+				//onpremise.WithTempDataCopy(true),
+				//SetTempDataDir is used to set the directory where the temporary data file will be copied
+				//default is os.TempDir() aka temporary directory of the OS
+				//onpremise.SetTempDataDir("/tmp"),
+			)
+
+			if err != nil {
+				log.Fatalf("Failed to create engine: %v", err)
+			}
+
+			//Process evidence
+			resultsHash, err := pl.Process(ExampleEvidence)
+			if err != nil {
+				log.Fatalf("Failed to process: %v", err)
+			}
+			defer resultsHash.Free()
+
+			//Get values from results
+			browser, err := resultsHash.ValuesString("BrowserName", ",")
+			if err != nil {
+				log.Fatalf("Failed to get BrowserName: %v", err)
+			}
+
+			log.Printf("BrowserName: %s", browser)
+
+			deviceType, err := resultsHash.ValuesString("DeviceType", ",")
+			if err != nil {
+				log.Fatalf("Failed to get DeviceType: %v", err)
+			}
+
+			log.Printf("DeviceType: %s", deviceType)
+
+			//use results and do detection
+			resultsHash.Free()
+
+			pl.Stop()
+
+			return nil
 		},
 	)
-	if err != nil {
-		log.Fatalf("Failed to process: %v", err)
-	}
-	defer resultsHash.Free()
-
-	//Get values from results
-	browser, err := resultsHash.ValuesString("BrowserName", ",")
-	if err != nil {
-		log.Fatalf("Failed to get BrowserName: %v", err)
-	}
-
-	log.Printf("BrowserName: %s", browser)
-
-	deviceType, err := resultsHash.ValuesString("DeviceType", ",")
-	if err != nil {
-		log.Fatalf("Failed to get DeviceType: %v", err)
-	}
-
-	log.Printf("DeviceType: %s", deviceType)
-
-	//use results and do detection
-	resultsHash.Free()
-	<-make(chan struct{})
-
-	pl.Stop()
 }
---
+++ onpremise/example_base.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/51Degrees/device-detection-go/v4/dd"
+	"github.com/51Degrees/device-detection-go/v4/onpremise"
+	"os"
+)
+
+type ExampleParams struct {
+	LicenceKey string
+	Product    string
+	DataFile   string
+}
+
+type ExampleFunc func(params ExampleParams) error
+
+func RunExample(exampleFunc ExampleFunc) {
+	params := ExampleParams{
+		LicenceKey: os.Getenv("LICENCE_KEY"),
+		Product:    os.Getenv("PRODUCT"),
+		DataFile:   "51Degrees-LiteV4.1.hash",
+	}
+
+	err := exampleFunc(params)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var ExampleEvidence = []onpremise.Evidence{
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Arch",
+		Value:  "x86",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Model",
+		Value:  "Intel",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Mobile",
+		Value:  "?0",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Platform",
+		Value:  "Windows",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Platform-Version",
+		Value:  "10.0",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua-Full-Version-List",
+		Value:  "58.0.3029.110",
+	},
+	{
+		Prefix: dd.HttpHeaderString,
+		Key:    "Sec-Ch-Ua",
+		Value:  `"\"Chromium\";v=\"91.0.4472.124\";a=\"x86\";p=\"Windows\";rv=\"91.0\""`,
+	},
+}
