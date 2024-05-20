0bd660750577a98309a503934f7de095530798e6
Eugene Dorfman <eugene.dorfman@gmail.com>
2024-05-20 09:38:04 +0200

    example updated to check  evidence before and after

--- onpremise/update_polling_interval.go
+++ onpremise/update_polling_interval.go
@@ -8,6 +8,17 @@ import (
 	"time"
 )
 
+func processExampleEvidence(engine *onpremise.Engine) {
+	//Process evidence
+	resultsHash, _ := engine.Process(common.ExampleEvidence)
+	defer resultsHash.Free()
+	//Get values from results
+	browser, _ := resultsHash.ValuesString("BrowserName", ",")
+	deviceType, _ := resultsHash.ValuesString("DeviceType", ",")
+	log.Printf("BrowserName: %s", browser)
+	log.Printf("DeviceType: %s", deviceType)
+}
+
 func main() {
 
 	common.RunExample(
@@ -17,17 +28,16 @@ func main() {
 			config := dd.NewConfigHash(dd.Balanced)
 
 			//Create on-premise engine
-			pl, err := onpremise.New(
+			engine, err := onpremise.New(
 				config,
-				//Provide your own file URL
-				//it can be compressed as gz or raw, engine will handle it
-				// Passing a custom update URL
-				// Path to your data file
 
+				// Path to your data file
 				onpremise.WithDataFile(params.DataFile),
+
 				// For automatic updates to work you will need to provide a license key.
 				// A license key can be obtained with a subscription from https://51degrees.com/pricing
 				onpremise.WithLicenseKey(params.LicenseKey),
+
 				// Enable automatic updates.
 				onpremise.WithAutoUpdate(true),
 
@@ -35,51 +45,42 @@ func main() {
 				// check for updates to data files. A recommended
 				// polling interval in a production environment is
 				// around 30 minutes.
-				onpremise.WithPollingInterval(10),
+				onpremise.WithPollingInterval(5),
+
 				// Set the max amount of time in seconds that should be
 				// added to the polling interval. This is useful in datacenter
 				// applications where multiple instances may be polling for
 				// updates at the same time. A recommended amount in production
 				// environments is 600 seconds.
 				onpremise.WithRandomization(1),
+
 				// Enable update on startup, the auto update system
 				// will be used to check for an update before the
 				// device detection engine is created.
 				onpremise.WithUpdateOnStart(false),
+
+				// Optionally provide your own file URL
+				// onpremise.WithDataUpdateUrl(""),
+
+				// Whether a temp copy should be created
+				onpremise.WithTempDataCopy(false),
 			)
 
 			if err != nil {
 				log.Fatalf("Failed to create engine: %v", err)
 			}
 
-			//Process evidence
-			resultsHash, err := pl.Process(common.ExampleEvidence)
-			if err != nil {
-				log.Fatalf("Failed to process: %v", err)
-			}
-			defer resultsHash.Free()
+			defer engine.Stop()
 
-			//Get values from results
-			browser, err := resultsHash.ValuesString("BrowserName", ",")
-			if err != nil {
-				log.Fatalf("Failed to get BrowserName: %v", err)
-			}
+			//process before file has been updated
+			processExampleEvidence(engine)
 
-			log.Printf("BrowserName: %s", browser)
+			<-time.After(20 * time.Second)
 
-			deviceType, err := resultsHash.ValuesString("DeviceType", ",")
-			if err != nil {
-				log.Fatalf("Failed to get DeviceType: %v", err)
-			}
-
-			log.Printf("DeviceType: %s", deviceType)
-
-			//use results and do detection
-			resultsHash.Free()
-
-			<-time.After(30 * time.Second)
-
-			pl.Stop()
+			//process again after the file has presumably been updated
+			processExampleEvidence(engine)
+			processExampleEvidence(engine)
+			processExampleEvidence(engine)
 
 			return nil
 		},
